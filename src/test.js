f7bec2311c24276165e7ec8b8a68f9f3a69e4dbc
bipulshakya <bipulshakya47@gmail.com>
2026-08-16 22:37:24 +0545

    Testing Server

--- src/test.js
+++ src/test.js
@@ -1,4 +1,20 @@
-const { createServer } = require('node:http');
+//Commonjs format
+/*const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});*/
+
+import { createServer } from 'node:http';
 
 const hostname = '127.0.0.1';
 const port = 3000;
