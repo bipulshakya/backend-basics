641c9f2119f95916138212f8809bcc3f9955a733
bipulshakya <bipulshakya47@gmail.com>
2026-08-17 20:58:38 +0545

    Created array of Four books

---
+++ src/data/books.js
@@ -0,0 +1,32 @@
+const books = [
+    {
+        id: 1,
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        year: 1960,
+        available: true
+    },
+    {
+        id: 2,
+        title: "Pride and Prejudice",
+        author: "Jane Austen",
+        Year: 1813,
+        available: true
+    },
+    {
+        id: 3,
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        year: 1925,
+        available: false
+    },
+    {
+        id: 4,
+        title: "1984",
+        author: "George Orwell",
+        year: 1949,
+        available: true
+    }
+];
+
+module.exports = books;
