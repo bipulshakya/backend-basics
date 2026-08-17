9ba4e8d0b91bf2abceb7814048395ca2a1b115f8
bipulshakya <bipulshakya47@gmail.com>
2026-08-17 23:34:40 +0545

    added GEt /health

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 //Health
-app.get("health", (req, res) => {
+app.get("/health", (req, res) => {
   res.status(200).json({
     status: "ok"
   });
